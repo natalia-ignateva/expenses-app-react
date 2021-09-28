2e9600f4f0c0a40fe3c7ebaab573608823ef118e
Natalia Ignateva <ignatyevanat@gmail.com>
2021-09-28 14:40:05 +0200

    add componets

---
+++ src/components/Expenses.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+import Card from './Card';
+
+function Expenses(props) {
+    return (
+        <Card className="expenses">
+            {props.expenses.map((el, i) => {
+                return (
+                    <ExpenseItem
+                        title={props.expenses[i].title}
+                        amount={props.expenses[i].amount}
+                        date={props.expenses[i].date}
+                    />
+                );
+            })}
+        </Card>
+    );
+}
+
+export default Expenses;
