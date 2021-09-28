4e9b9ddaf6662da05189e49f4122f9ab7da41ece
Natalia Ignateva <ignatyevanat@gmail.com>
2021-09-28 16:33:47 +0200

    refactor code structure

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ExpenseDate.css';
 
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
     const day = props.date.toLocaleString('en-GB', { day: 'numeric' });
     const month = props.date.toLocaleString('en-GB', { month: 'long' });
     const year = props.date.getFullYear();
@@ -13,6 +13,6 @@ function ExpenseDate(props) {
             <div className="expense-date__year">{year}</div>
         </div>
     );
-}
+};
 
 export default ExpenseDate;
